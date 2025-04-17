7b6a7b00af89be152fc64455c933c929de4ec6a7
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-04-17 19:58:11 +0200

    Build a Palindrome Checker
    JavaScript Algorithms and Data Structures - Build a Palindrome Checker

---
+++ src/2. JavaScript Algorithms and Data Structures/2.1. Build a Palindrome Checker/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const textInput = document.getElementById("text-input");
+  const checkBtn = document.getElementById("check-btn");
+  const result = document.getElementById("result");
+
+  checkBtn.addEventListener("click", function () {
+    const inputText = textInput.value;
+
+    if (!inputText) {
+      alert("Please input a value");
+      return;
+    }
+
+    const isPalindrome = checkPalindrome(inputText);
+
+    result.textContent = `${inputText} is ${
+      isPalindrome ? "a" : "not a"
+    } palindrome`;
+  });
+
+  function checkPalindrome(str) {
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+    const reversedStr = cleanedStr.split("").reverse().join("");
+    return cleanedStr === reversedStr;
+  }
+});
