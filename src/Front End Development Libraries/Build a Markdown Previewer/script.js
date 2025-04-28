7f2a2b6371480dfd0baaad39aab186e807619b79
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-04-28 20:56:39 +0200

    Build a Markdown Previewer
    Front End Development Libraries - Build a Markdown Previewer

---
+++ src/Front End Development Libraries/Build a Markdown Previewer/script.js
@@ -0,0 +1,35 @@
+const defaultMarkdown = `# Heading (H1)
+## Sub Heading (H2)
+[Visit freeCodeCamp](https://www.freecodecamp.org)
+\`inline code\`
+
+\`\`\`
+// code block
+function hello() {
+    console.log("Hello World!");
+}
+\`\`\`
+
+- List item 1
+- List item 2
+- List item 3
+
+> Blockquote example
+
+![Logo](https://cdn.freecodecamp.org/platform/universal/fcc_primary.svg)
+
+**Bolded Text**`;
+
+const editor = document.getElementById("editor");
+const preview = document.getElementById("preview");
+
+marked.setOptions({
+  breaks: true,
+});
+
+editor.value = defaultMarkdown;
+preview.innerHTML = marked.parse(defaultMarkdown);
+
+editor.addEventListener("input", (e) => {
+  preview.innerHTML = marked.parse(e.target.value);
+});
