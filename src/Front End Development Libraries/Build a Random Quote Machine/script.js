774eb2396c99d164dd478b3b4c630902e1570bb8
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-04-28 02:02:35 +0200

    Build a Random Quote Machine
    Front End Development Libraries - Build a Random Quote Machine

---
+++ src/Front End Development Libraries/Build a Random Quote Machine/script.js
@@ -0,0 +1,87 @@
+window.onload = go;
+function go() {
+  $("#new-quote").click(function () {
+    genRandQuote();
+  });
+
+  $("#tweet-quote").click(function () {});
+
+  genRandQuote();
+}
+
+function genRandQuote() {
+  var quotes = [
+    {
+      id: 1,
+      text: `"A rose by any other name would smell as sweet"`,
+      source: `- William Shakespeare`,
+    },
+    {
+      id: 2,
+      text: `"All that glitters is not gold"`,
+      source: `- William Shakespeare`,
+    },
+    {
+      id: 3,
+      text: `"All the world’s a stage, and all the men and women merely players"`,
+      source: `- William Shakespeare`,
+    },
+    {
+      id: 4,
+      text: `"Ask not what your country can do for you; ask what you can do for your country"`,
+      source: `- John Kennedy`,
+    },
+    {
+      id: 5,
+      text: `"Eighty percent of success is showing up"`,
+      source: `- Woody Allen`,
+    },
+    {
+      id: 6,
+      text: `"Elementary, my dear Watson"`,
+      source: `- Woody Allen`,
+    },
+    {
+      id: 7,
+      text: `"Genius is one percent inspiration and ninety-nine percent perspiration"`,
+      source: `- Thomas Edison`,
+    },
+    {
+      id: 8,
+      text: `"If you are going through hell, keep going"`,
+      source: `- Winston Churchill`,
+    },
+    {
+      id: 9,
+      text: `"If you want something said, ask a man; if you want something done, ask a woman"`,
+      source: `- Margaret Thatcher`,
+    },
+    {
+      id: 10,
+      text: `"Life is like riding a bicycle. To keep your balance, you must keep moving"`,
+      source: `- Albert Einstein`,
+    },
+  ];
+  var randQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  $("#text").html(randQuote.text);
+  $("#author").html(randQuote.source);
+  $("#tweet-quote").attr(
+    "href",
+    stringToClickToTweetURL('"' + randQuote.text + '" - ' + randQuote.source)
+  );
+}
+
+function stringToClickToTweetURL(str) {
+  // Convert to Click to Tweet URL
+  var stringToConvert = str
+    .split(" ")
+    .join("%20")
+    .split("@")
+    .join("%40")
+    .split("!")
+    .join("%21");
+
+  var resultString = "https://twitter.com/intent/tweet?text=" + stringToConvert;
+
+  return resultString;
+}
