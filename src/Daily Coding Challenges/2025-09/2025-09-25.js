3d8b909bda6674ec4747fc9ee5ed7b023a2b6a59
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-09-25 13:00:37 +0200

    Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2025-09/2025-09-25.js
@@ -0,0 +1,13 @@
+/**
+
+2nd Largest
+
+Given an array, return the second largest distinct number.
+
+*/
+
+function secondLargest(arr) {
+  let unique = [...new Set(arr)];
+  unique.sort((a, b) => b - a);
+  return unique.length >= 2 ? unique[1] : null;
+}
