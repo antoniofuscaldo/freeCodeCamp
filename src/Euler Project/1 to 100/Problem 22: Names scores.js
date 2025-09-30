3c9fa4aa87e806c99f778b1687b0d09d8daa3e54
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-10-01 01:16:46 +0200

    Update: Euler Project

---
+++ src/Euler Project/1 to 100/Problem 22: Names scores.js
@@ -0,0 +1,31 @@
+/**
+
+Problem 22: Names scores
+
+Using names, an array defined in the background containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the array?
+
+*/
+
+function namesScores(arr) {
+  arr.sort();
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let name = arr[i].replace(/"/g, "");
+    let score = 0;
+    for (let j = 0; j < name.length; j++) {
+      score += name.charCodeAt(j) - 64;
+    }
+    total += score * (i + 1);
+  }
+  return total;
+}
+
+// Only change code above this line
+const test1 = ["THIS", "IS", "ONLY", "A", "TEST"];
+const test2 = ["I", "REPEAT", "THIS", "IS", "ONLY", "A", "TEST"];
+
+namesScores(test1);
---
+++ src/Euler Project/1 to 100/Problem 24: Lexicographic permutations.js
@@ -0,0 +1,34 @@
+/**
+
+Problem 24: Lexicographic permutations
+
+A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+What is the nth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+*/
+
+function lexicographicPermutations(n) {
+  let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let k = n;
+  let result = 0;
+
+  function factorial(x) {
+    let f = 1;
+    for (let i = 2; i <= x; i++) f *= i;
+    return f;
+  }
+
+  for (let i = digits.length; i > 0; i--) {
+    let f = factorial(i - 1);
+    let index = Math.floor(k / f);
+    result = result * 10 + digits[index];
+    digits.splice(index, 1);
+    k %= f;
+  }
+
+  return result;
+}
+
+lexicographicPermutations(999999);
