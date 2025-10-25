fcbdb593939f1a9fc0051c647b996b8b71c29124
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-10-26 01:07:42 +0200

    Upload Daily Coding Challenges

---
+++ src/Daily Coding Challenges/2025-10/2025-10-24.js
@@ -0,0 +1,52 @@
+/**
+
+Hidden Treasure
+
+Given a 2D array representing a map of the ocean floor that includes a hidden treasure, and an array with the coordinates ([row, column]) for the next dive of your treasure search, return "Empty", "Found", or "Recovered" using the following rules:
+
+The given 2D array will contain exactly one unrecovered treasure, which will occupy multiple cells.
+
+Each cell in the 2D array will contain one of the following values:
+
+"-": No treasure.
+"O": A part of the treasure that has not been found.
+"X": A part of the treasure that has already been found.
+If the dive location has no treasure, return "Empty".
+
+If the dive location finds treasure, but at least one other part of the treasure remains unfound, return "Found".
+
+If the dive location finds the last unfound part of the treasure, return "Recovered".
+
+For example, given:
+
+[
+  [ "-", "X"],
+  [ "-", "X"],
+  [ "-", "O"]
+]
+And [2, 1] for the coordinates of the dive location, return "Recovered" because the dive found the last unfound part of the treasure.
+
+*/
+
+function dive(map, coordinates) {
+  const [row, col] = coordinates;
+  const cell = map[row][col];
+
+  if (cell === "-") {
+    return "Empty";
+  }
+
+  if (cell === "O") {
+    map[row][col] = "X";
+  }
+
+  for (let r = 0; r < map.length; r++) {
+    for (let c = 0; c < map[r].length; c++) {
+      if (map[r][c] === "O") {
+        return "Found";
+      }
+    }
+  }
+
+  return "Recovered";
+}
---
+++ src/Daily Coding Challenges/2025-10/2025-10-25.js
@@ -0,0 +1,20 @@
+/**
+
+Complementary DNA
+
+Given a string representing a DNA sequence, return its complementary strand using the following rules:
+
+DNA consists of the letters "A", "C", "G", and "T".
+The letters "A" and "T" complement each other.
+The letters "C" and "G" complement each other.
+For example, given "ACGT", return "TGCA".
+
+*/
+
+function complementaryDNA(strand) {
+  const pairs = { A: "T", T: "A", C: "G", G: "C" };
+  return strand
+    .split("")
+    .map((ch) => pairs[ch])
+    .join("");
+}
