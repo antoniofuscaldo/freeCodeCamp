b28c6b35969c594a3be5408d0a8c36fa27fec216
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-12-02 16:10:49 +0100

    Upload Daily Coding Challenges

---
+++ src/Daily Coding Challenges/2025-12/2025-12-01.js
@@ -0,0 +1,15 @@
+/**
+
+Miles to Kilometers
+
+Given a distance in miles as a number, return the equivalent distance in kilometers.
+
+The input will always be a non-negative number.
+1 mile equals 1.60934 kilometers.
+Round the result to two decimal places.
+
+*/
+
+function convertToKm(miles) {
+  return +(miles * 1.60934).toFixed(2);
+}
