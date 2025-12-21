8391c5bf4ad17a2c1c563708d1d6c3f0c2d4dad8
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-12-21 12:59:42 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2025-12/2025-12-21.js
@@ -0,0 +1,52 @@
+/**
+
+Daylight Hours
+
+December 21st is the winter solstice for the northern hemisphere and the summer solstice for the southern hemisphere. That means it's the day with the least daylight in the north and the most daylight in the south.
+
+Given a latitude number from -90 to 90, return a rough approximation of daylight hours on the solstice using the following table:
+
+Latitude	Daylight Hours
+-90	24
+-75	23
+-60	21
+-45	15
+-30	13
+-15	12
+0	12
+15	11
+30	10
+45	9
+60	6
+75	2
+90	0
+If the given latitude does not exactly match a table entry, use the value of the closest latitude.
+
+*/
+
+function daylightHours(latitude) {
+  const table = [
+    { lat: -90, hours: 24 },
+    { lat: -75, hours: 23 },
+    { lat: -60, hours: 21 },
+    { lat: -45, hours: 15 },
+    { lat: -30, hours: 13 },
+    { lat: -15, hours: 12 },
+    { lat: 0, hours: 12 },
+    { lat: 15, hours: 11 },
+    { lat: 30, hours: 10 },
+    { lat: 45, hours: 9 },
+    { lat: 60, hours: 6 },
+    { lat: 75, hours: 2 },
+    { lat: 90, hours: 0 },
+  ];
+
+  let closest = table[0];
+  for (const entry of table) {
+    if (Math.abs(entry.lat - latitude) < Math.abs(closest.lat - latitude)) {
+      closest = entry;
+    }
+  }
+
+  return closest.hours;
+}
