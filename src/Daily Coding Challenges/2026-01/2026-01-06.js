6464c3ce667b7a4a381b774dfc77cb5b6a1b2a32
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-01-06 08:56:05 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-01/2026-01-06.js
@@ -0,0 +1,31 @@
+/** 
+
+vOwElcAsE
+
+Given a string, return a new string where all vowels are converted to uppercase and all other alphabetical characters are converted to lowercase.
+
+Vowels are "a", "e", "i", "o", and "u" in any case.
+Non-alphabetical characters should remain unchanged.
+
+*/
+
+function vowelCase(str) {
+  const vowels = 'aeiou';
+
+  return str
+    .split('')
+    .map((char) => {
+      const lower = char.toLowerCase();
+
+      if (vowels.includes(lower)) {
+        return char.toUpperCase();
+      }
+
+      if (/[a-z]/i.test(char)) {
+        return char.toLowerCase();
+      }
+
+      return char;
+    })
+    .join('');
+}
