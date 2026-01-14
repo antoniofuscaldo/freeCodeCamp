c2b465fcc9d2c39e0dbc74f5e2a0d56dbe12ac24
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-01-14 02:37:32 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-01/2026-01-13.js
@@ -0,0 +1,10 @@
+/**
+ Odd or Even?
+
+ Given a positive integer, return "Odd" if it's an odd number, and "Even" is it's even.
+
+*/
+
+function oddOrEven(n) {
+  return n % 2 === 0 ? 'Even' : 'Odd';
+}
