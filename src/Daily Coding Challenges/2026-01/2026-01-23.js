ca2004ee46c0e604347410961d8c40ef399840f5
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-01-23 12:47:28 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-01/2026-01-23.js
@@ -0,0 +1,15 @@
+/**
+ *
+Hex Validator
+
+Given a string, determine whether it is a valid CSS hex color. A valid CSS hex color must:
+
+Start with a #, and
+be followed by either 3 or 6 hexadecimal characters.
+Hexadecimal characters are numbers 0 through 9 and letters a through f (case-insensitive).
+
+*/
+
+function isValidHex(str) {
+  return /^#([0-9a-f]{3}|[0-9a-f]{6})$/i.test(str);
+}
