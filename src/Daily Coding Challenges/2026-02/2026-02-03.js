2d2e927b56086aa90cdb707e37a4aeb49a09abd6
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-02-04 09:14:20 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-02/2026-02-03.js
@@ -0,0 +1,11 @@
+/**
+
+String Mirror
+
+Given a string, return a new string that consists of the given string with a reversed copy of itself appended to the end of it.
+
+*/
+
+function mirror(str) {
+  return str + str.split('').reverse().join('');
+}
