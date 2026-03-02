e2c4811ce78f96afa6be4c9bf01bf26d647efb36
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-03-02 16:47:59 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-03/2026-03-02.js
@@ -0,0 +1,24 @@
+/**
+
+Sum the Letters
+
+Given a string, return the sum of its letters.
+
+Letters are A-Z in uppercase or lowercase
+Letter values are: "A" = 1, "B" = 2, ..., "Z" = 26
+Uppercase and lowercase letters have the same value.
+Ignore all non-letter characters.
+
+*/
+
+const sumLetters = (s) =>
+  [...s].reduce(
+    (a, c) =>
+      a +
+      (c >= 'A' && c <= 'Z'
+        ? c.charCodeAt(0) - 64
+        : c >= 'a' && c <= 'z'
+          ? c.charCodeAt(0) - 96
+          : 0),
+    0
+  );
