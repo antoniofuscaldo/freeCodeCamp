b2c9ed8cc9efcec22ed1882af960f1e7921097ae
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-03-03 13:51:29 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-03/2026-03-03.js
@@ -0,0 +1,22 @@
+/**
+
+Perfect Cube Count
+
+Given two integers, determine how many perfect cubes exist in the range between and including the two numbers.
+
+The lower number is not garanteed to be the first argument.
+A number is a perfect cube if there exists an integer (n) where n * n * n = number. For example, 27 is a perfect cube because 3 * 3 * 3 = 27.
+
+*/
+
+function countPerfectCubes(a, b) {
+  const lo = Math.min(a, b);
+  const hi = Math.max(a, b);
+  let c = 0;
+
+  for (let n = Math.cbrt(lo) | 0; n * n * n <= hi; n++) {
+    if (n * n * n >= lo) c++;
+  }
+
+  return c;
+}
