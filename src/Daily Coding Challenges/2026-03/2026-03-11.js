51661330ce5baf338cedd33571fbdab3627c155d
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-03-11 12:36:27 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-03/2026-03-11.js
@@ -0,0 +1,18 @@
+/**
+
+Word Length Converter
+
+Given a string of words, return a new string where each word is replaced by its length.
+
+Words in the given string will be separated by a single space
+Keep the spaces in the returned string.
+For example, given "hello world", return "5 5".
+
+*/
+
+function convertWords(str) {
+  return str
+    .split(' ')
+    .map((word) => word.length)
+    .join(' ');
+}
