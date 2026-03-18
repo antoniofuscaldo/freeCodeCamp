8586d7715a16789dec4a5fb971d94a7adc26f52e
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-03-18 16:44:32 +0100

    Upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-03/2026-03-18.js
@@ -0,0 +1,14 @@
+/**
+
+Largest Number
+
+Given a string of numbers separated by various punctuation, return the largest number.
+
+The given string will only contain numbers and separators.
+Separators can be commas (","), exclamation points ("!"), question marks ("?"), colons (":"), or semi-colons (";").
+
+*/
+
+function largestNumber(str) {
+  return Math.max(...str.split(/[,!?:;]+/).map(Number));
+}
