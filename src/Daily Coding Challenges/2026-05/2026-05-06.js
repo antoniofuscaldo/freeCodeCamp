c804e3992a1bae66d19ab475d63737b1df86ef4b
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-05-06 14:47:15 +0200

    feat: upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-05/2026-05-06.js
@@ -0,0 +1,17 @@
+/**
+
+Allergen Friendly Meals
+
+Given an array of meals and an array of allergens to avoid, return the names of all the meals that contain none of the given allergens.
+
+Each meal is in the format [meal, allergens], where meal is the name of the meal, and allergens is an array of the allergens the meal contains. For example, ["pasta", ["wheat", "milk"]].
+Allergens to avoid will be an array of strings.
+Return safe meal names in the same order given. If no meal is safe, return an empty array.
+
+*/
+
+function getAllergenFriendlyMeals(meals, allergens) {
+  return meals
+    .filter(([_, a]) => !a.some((x) => allergens.includes(x)))
+    .map(([name]) => name);
+}
