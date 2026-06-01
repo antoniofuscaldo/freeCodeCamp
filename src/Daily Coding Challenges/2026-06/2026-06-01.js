9b6fcb4b262f98ad9e9feeb08020a13c3d27b970
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2026-06-01 14:28:30 +0200

    feat: upload Daily Coding Challenge

---
+++ src/Daily Coding Challenges/2026-06/2026-06-01.js
@@ -0,0 +1,16 @@
+/**
+
+Schema Validator Part 1
+
+Given an object (JavaScript) or dictionary (Python), determine if it matches the following schema:
+
+{
+  username: string
+}
+Extra keys are allowed
+
+*/
+
+function isValidSchema(obj) {
+  return typeof obj.username === 'string';
+}
